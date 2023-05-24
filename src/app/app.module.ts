fb14da1286f7eddf5521f2f1cb51fba49fd3c1bb
Rithik105 <100821485+Rithik105@users.noreply.github.com>
2023-05-24 13:29:14 +0530

    base models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DetailsComponent } from './details/details.component';
+import { TitleContainerComponent } from './title-container/title-container.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { SimilarToThisComponent } from './similar-to-this/similar-to-this.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DetailsComponent,
+    TitleContainerComponent,
+    MainContainerComponent,
+    SimilarToThisComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/details/title-container/title-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-container',
+  templateUrl: './title-container.component.html',
+  styleUrls: ['./title-container.component.css']
+})
+export class TitleContainerComponent {
+
+}
