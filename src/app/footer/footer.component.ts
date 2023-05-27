06b98b65fc030c3ca3f1b9906b42567aec9b806c
Rithik105 <100821485+Rithik105@users.noreply.github.com>
2023-05-28 01:17:01 +0530

    footer done

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+footerList=[
+  {
+    title:'Must Watch Movies',
+    content:['Masooda','Sardar','Ori Devuda','Swathi Muthyam','DJ Tillu']
+  },
+  {
+    title:'Popular Telugu Movies',
+    content:['Pakka Commercial','Love Story','Colour Photo','Krack','Sulthan']
+  },
+  {
+    title:'Genres',
+    content:['Telugu Drama Movies','Telugu Comedy Movies','Telugu Action Movies','Telugu Romance Movies'
+      ]
+  },
+  {
+    title:'Learn More',
+    content:['View Plans','About Us','FAQs / Help','Privacy Policy','Terms of Service','Grievance Redressal','Blog']
+  }
+]
 }
